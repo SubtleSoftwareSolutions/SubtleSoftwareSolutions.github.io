c5a3d7d9045ed778bd19f55afc566f73e5960842
Marta Zażlak <marta.zazlak@hotmail.com>
2017-03-02 23:22:36 +0100

    Part one

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    var MODEL = {} || MODEL,
+        VIEW = {} | VIEW,
+        CONTROL = {} || CONTROL;
+
+/*
+ * MODEL
+ */
+    function Model() {
+        if(!(this instanceof Model)) {
+            return new Model();
+        }
+        this.started = "https://api.github.com/";
+    }
+    Model.prototype = {
+        getGithubURL: function(username) {
+            return this.started + "users/" + username + "/repos";
+        }
+    };
+
+/*
+ * CONTROL
+ */
+    function Control(model, view) {
+        if(!(this instanceof Control)) {
+            return new Control();
+        }
+        this.model = model;
+        this.view = view;
+    }
+    Control.prototype = {
+        createAllCards: function(username) {
+            var that = this;
+            $.ajax({
+                dataType: 'json',
+                url: this.model.getGithubURL(username),
+                success: function createAll(data) {
+                    that.view.createCards(data);
+                }
+            });
+        }
+    };
+
+
+
+    function View(cardParent) {
+        if(!(this instanceof View)) {
+            return new View();
+        }
+        this.cardParent = cardParent;
+    }
+    View.prototype = {
+        createOneCard: function(onePartData) {
+            var htmlCard = '<div class="card"><div class="card-block"><a href="' + onePartData.url + '" target="_blank"><h4 class="card-title">' + onePartData.name + '</h4></a><h6 class="card-subtitle mb-2 text-muted">author: ' + onePartData.owner.login + '</h6><p class="card-text">' + onePartData.description + '</p></div></div>';
+            return htmlCard;
+        },
+        createCards: function(json) {
+            var that = this;
+            var tmp = '';
+            var maxCount = 3;
+            var len = parseInt(json.length);
+            var result  = 0;
+
+            for(var i = 1; i < len + 1; i++) {
+                tmp += that.createOneCard(json[i - 1]);
+                if(i % maxCount === 0) {
+                    addGroup(tmp);
+                    tmp = '';
+                    console.log('Zerowanko: ' + tmp);
+                }
+            }
+
+            function addGroup(content) {
+                var str = '<div class="card-group">';
+                str += content;
+                str += '</div>';
+                that.cardParent.append(str);
+            }
+        }
+    };
+
+//    LOGIC PART
+    MODEL = new Model();
+    VIEW = new View($('#card-parent'));
+    CONTROL = new Control(MODEL, VIEW);
+    CONTROL.createAllCards('ndv66');
+
+});
